ef41c70af239717b2c4fbf2fee991ea4560c3bab
Nikita Lysenko <nikitlysenko@gmail.com>
2022-06-07 18:37:10 +0300

    Update router_WEB3.sh

--- router_WEB3.sh
+++ router_WEB3.sh
@@ -37,11 +37,8 @@ function Install_docker {
     then
     echo " "
     echo -e "\e[1m\e[32mInstalling Docker ... \e[0m" && sleep 1
-    sudo apt-get install ca-certificates curl gnupg lsb-release -y
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
     fi
     echo " "
     echo -e "\e[1m\e[32mInstalling Docker Compose v2.5.1 ... \e[0m" && sleep 1
