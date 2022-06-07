ae98ea40e9fd9618e4e0bb79ff5709d3a23d53cd
Nikita Lysenko <nikitlysenko@gmail.com>
2022-06-07 18:24:37 +0300

    Update router_WEB3.sh

--- router_WEB3.sh
+++ router_WEB3.sh
@@ -45,7 +45,9 @@ function Install_docker {
     fi
     echo " "
     echo -e "\e[1m\e[32mInstalling Docker Compose v2.5.1 ... \e[0m" && sleep 1
-    apt install docker-compose
+    curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 }
 
 function install_nxtp {
